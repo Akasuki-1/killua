6cc642747b23211aaf49bcb38c3e948bb7e28334
Akasuki-1 <79846938+Akasuki-1@users.noreply.github.com>
2021-03-26 10:22:05 +0530

    Initial commit

---
+++ MashaRoBot/modules/___masha_tools.py
@@ -0,0 +1,18 @@
+# © @Mr_Dark_Prince
+
+__mod_name__ = "▫️MASHAROBOT SPECIAL TOOLS▫️"
+
+__help__ = """
+ ❍ /song [song name] :uploads the song in it's best quality available
+ 💡Ex: `/song Faded Alan Walker`
+
+ ❍ /github [username] :Get information about a GitHub user.
+
+ ❍ /country [country name] :Gathering info about given country
+
+ ❍ Masha [query] :Masha answers the query
+  💡Ex: `Masha where is India?`
+
+ ❍ /encrypt: Encrypts The Given Text
+ ❍ /decrypt: Decrypts Previously Ecrypted Text
+"""
